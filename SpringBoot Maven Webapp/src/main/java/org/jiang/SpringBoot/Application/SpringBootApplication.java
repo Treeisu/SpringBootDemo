b99f149ffc67cb30afa293a745383db48eb388f4
Treeisu <cherryjiangbiao@qq.com>
2017-05-11 18:14:32 +0800

    springboot

---
+++ SpringBoot Maven Webapp/src/main/java/org/jiang/SpringBoot/Application/SpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.jiang.SpringBoot.Application;
+
+import org.springframework.boot.SpringApplication;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class SpringBootApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(SpringBootApplication.class, args);
+	}
+
+}
---
+++ SpringBoot Maven Webapp/src/main/java/org/jiang/SpringBoot/Application/sampleController.java
@@ -0,0 +1,23 @@
+package org.jiang.SpringBoot.Application;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+@RestController
+@EnableAutoConfiguration
+public class sampleController {
+	@Value(value = "")
+	private String cupSzie;
+	@RequestMapping("/say")
+	@ResponseBody
+	public String firstTest(){
+		
+		return "Hello World!!!  This is SpringBoot~";		
+	}
+}
